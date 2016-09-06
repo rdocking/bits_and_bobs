0a08591b7e9d19afeac6f386516e99ae6c0f5e99
Rod Docking <rdocking@bcgsc.ca>
2016-09-06 13:04:49 -0700

    Add python skeleton

---
+++ daily_quotes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+daily_quotes.py
+
+Created by Rod Docking on 2016-09-06.
+"""
+
+import sys
+import os
+
+
+def main():
+    """Print a semi-random collection of quotes"""
+
+
+
+if __name__ == '__main__':
+    main()
