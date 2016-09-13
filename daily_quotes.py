af239e40bf845723cfa81da0206139688c991430
Rod Docking <rdocking@bcgsc.ca>
2016-09-13 14:26:59 -0700

    Flesh out daily quotes script

--- daily_quotes.py
+++ daily_quotes.py
@@ -7,12 +7,18 @@ Created by Rod Docking on 2016-09-06.
 """
 
 import sys
-import os
+import json
 
 
 def main():
     """Print a semi-random collection of quotes"""
 
+    # Read in the JSON set of quotes
+    with open(sys.argv[1]) as data_file:
+        data = json.load(data_file)
+    # Sort and the quotes into order
+    quotes_to_display = data['first']
+    print quotes_to_display
 
 
 if __name__ == '__main__':
